9ca866872617a698bfd8dcfeb3c3367f0f55a844
lewdogman <89322336+lewdogman@users.noreply.github.com>
2021-08-23 14:26:40 +0100

    lol

--- include/PlatformMonkeWatchView.hpp
+++ include/PlatformMonkeWatchView.hpp
@@ -12,8 +12,7 @@ DECLARE_CLASS_CUSTOM(PlatformMonke, PlatformMonkeWatchView, GorillaUI::Component
     DECLARE_INSTANCE_METHOD(void, DrawHeader);
     DECLARE_INSTANCE_METHOD(void, DrawBody);
     DECLARE_INSTANCE_METHOD(void, OnKeyPressed, int key);
-    DECLARE_INSTANCE_METHOD(void, OnEnter, int key);
-    public:
+    DECLARE_INSTANCE_METHOD(void, OnEnter, int key)
         GorillaUI::UISelectionHandler* settingSelector = nullptr;
         GorillaUI::UIToggleInputHandler* toggleHandler = nullptr;
         GorillaUI::UISelectionHandler* leftPlatSizeSelector = nullptr;
